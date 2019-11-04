a666764e6b5bd77a489fd7342e8ed218b544da72
morgantatums <morgantatums@zillowgroup.com>
2019-11-04 15:51:55 -0800

    fixed some starter code lint errors

--- public/bauth-form.js
+++ public/bauth-form.js
@@ -37,21 +37,21 @@ const basicAuth = async e => {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        Authorization: bAuthData
-      }
+        Authorization: bAuthData,
+      },
     });
 
     // Grab the response body as JSON
     let authData = await authResponse.json();
 
     sendMessage(
-      `Received response:\n${authResponse.status} ${authResponse.statusText}`
+      `Received response:\n${authResponse.status} ${authResponse.statusText}`,
     );
 
     // If we got a successful response, we should have a token
     if (authResponse.status === 200) {
       sendMessage(
-        `Successfully logged in and recieved token:\n${authData.token}`
+        `Successfully logged in and recieved token:\n${authData.token}`,
       );
       console.log(authData.token);
     }
@@ -86,7 +86,7 @@ const sendMessage = message => {
 
   $('#bAuth-message').append(toast);
   $('#' + toastId).toast({
-    delay: 3000
+    delay: 3000,
   });
 
   $('#' + toastId).toast('show');
